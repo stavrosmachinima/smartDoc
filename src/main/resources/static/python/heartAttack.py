6c4d1948fbb1366bd588e838768f5643c78445d9
stpeppas41@gmail.com <stpeppas41@gmail.com>
2022-09-11 20:14:31 +0300

    fixed python model prediction bug. Needs more testing

--- src/main/resources/static/python/heartAttack.py
+++ src/main/resources/static/python/heartAttack.py
@@ -3,11 +3,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
 import joblib
 
 def findColumnQM(df):
@@ -52,44 +58,70 @@ def heatMap(df):
 
 def logic():
 	columns=['Age','Sex','ChestPain','RestBP','Chol','Fbs','RestECG','MaxHR','ExAng','Oldpeak','Slope','Ca','Thal','AHD']
-	df=pd.read_csv("processed.cleveland.data",names=columns)
+	df=pd.read_csv("../data/processed.cleveland.data",names=columns)
 	df.loc[df['AHD']>1,'AHD']=1
 
 	# find most common values per column to combat '?' values
 	# QM = Question Mark
 	replaceQMwithMode(df)
+	df['Ca']=df['Ca'].astype(float)
+	df['Thal']=df['Thal'].astype(float)
 
 	X=df.drop(['AHD'],axis=1).values
-	y=df['AHD'].values
-	scale=StandardScaler()
-	X=scale.fit_transform(X)
-	X_train, X_test, Y_train, Y_test = train_test_split(X,y,test_size=0.3, random_state=42)
-	model=RandomForestClassifier()
+	Y=df['AHD'].values
+
+	# print(np.count_nonzero(Y==0))
+	# print(np.count_nonzero(Y==1))
+	# Apo ta parapanw apotelesmata diakrinoume oti kamia ta3h den einai overrepresented. Ara dn xreiazetai na xrhsimopoihsoume stratifiedKfold
+
+	# *********** StandardScaler comes into play when the characteristics of the input dataset differ greatly between their ranges, or simply when they are measured in different units of measure. *******
+	# scale=StandardScaler()
+	# X=scale.fit_transform(X)
+	# print('ScaledX:'+str(X))
+	X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3, random_state=42)
+	model=LinearDiscriminantAnalysis()
 	model.fit(X_train,Y_train)
 	Y_pred=model.predict(X_test)
+
+	testArray=[41.0,0.0,3.0,112.0,268.0,0.0,2.0,172.0,1.0,0.0,1.0,0.0,3.0]
+
+	print(model.predict(np.asarray(testArray).reshape(1,-1)))
 
 	# ****** Accuracy ********
 
 	score=round(accuracy_score(Y_pred,Y_test)*100,2)
-	print("Accuracy score using Random Forest Classifier is: "+str(score)+" %")
+	print("Accuracy of LinearDiscriminantAnalysis is: "+str(score)+"%")
 
 
 	# ****** Cross validation Scores *******
 	k_folds=10
-	scores_train=cross_val_score(model,X_train,Y_train,cv=k_folds)
+	scores_train=cross_val_score(model,X_train,Y_train,cv=k_folds,scoring='accuracy')
 
 
-	print("Cross Validation Train Scores: ", scores_train)
-	print("Average Train CV Score: ", scores_train.mean())
+	# #print("Cross Validation Train Scores: ", scores_train)
+	print("Average Train CV Score: "+ str(round(scores_train.mean()*100,2))+"%")
 
-	scores_test=cross_val_score(model,X_train,Y_train,cv=k_folds)
+	scores_test=cross_val_score(model,X_train,Y_train,cv=k_folds,scoring='accuracy')
 
-	print("Cross Validation Test Scores: ", scores_test)
-	print("Average Test CV Score: ", scores_test.mean())
+	# #print("Cross Validation Test Scores: ", scores_test)
+	print("Average Test CV Score: "+ str(round(scores_test.mean()*100,2))+'%')
 
-	# ****** Save the model ****
-
+	# # ****** Save the model ****
+	print('Saving the model...')
 	joblib.dump(model,'Heart_model.pkl')
+
+	models=[]
+	models.append(('RFC',RandomForestClassifier()))
+	models.append(('LR',LogisticRegression(solver='liblinear',multi_class='ovr')))
+	models.append(('LDA',model))
+	models.append(('KNN',KNeighborsClassifier()))
+	models.append(('CART', DecisionTreeClassifier()))
+	models.append(('NB', GaussianNB()))
+	models.append(('SVM', SVC(gamma='auto')))
+	for name,model in models:
+		cv_results=cross_val_score(model,X_train,Y_train,cv=k_folds,scoring='accuracy')
+		print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
 
 if __name__ == "__main__":
 	logic()  
